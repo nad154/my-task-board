4236b8af0fe9e3f1b0a8a9001040d7ba73df311d
nad154 <119729778+nad154@users.noreply.github.com>
2026-07-28 15:10:53 +0700

    base UI

--- task-board-app/src/App.tsx
+++ task-board-app/src/App.tsx
@@ -2,23 +2,78 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import { type TaskStatus } from './types/taskType'
+
 import './App.css'
 
 
 import { TaskCard } from './components/TaskCard'
 
+const mockTasks = [
+  {
+    taskName: "Design System Migration",
+    taskDescription: "Update core components to match new Tailwind branding guidelines.",
+    taskStatus: "In Progress",
+    taskIcon: "AlarmClock"
+  },
+  {
+    taskName: "Fix Authentication Bug",
+    taskDescription: "Resolve token refresh failure when session expires in mobile view.",
+    taskStatus: "To Do",
+    taskIcon: "Balloon"
+  },
+  {
+    taskName: "Database Backup Automation",
+    taskDescription: "Configure nightly cron job to export PostgreSQL dumps to S3 bucket.",
+    taskStatus: "Completed",
+    taskIcon: "Album"
+  },
+  {
+    taskName: "Legacy API Clean Up",
+    taskDescription: null, // Testing optional null case
+    taskStatus: "Cancelled",
+    taskIcon: "Bean"
+  },
+  {
+    taskName: "User Onboarding Survey",
+    taskDescription: "Implement 3-step modal survey for new registered users.",
+    taskStatus: "To Do",
+    taskIcon: "AlarmClock", 
+  },
+];
+
 function App() {
 
+  function toggleTaskDetails(task_id: string) {
+    console.log("task id: ", {task_id}); 
+  }
+
   return (
-    <div >
-      <div className="flex-1 items-center text-center mt-10">
-        <p className="font-bold">TITLE</p>
+    <div className="w">
+      <div className="flex flex-col items-center mt-10 ">
+        <div className="flex w-100 ">
+          <h1 className="font-bold text-3xl">BOARD TITLE</h1>
+        </div>
+        <div className="flex w-100 mt-2">
+          <h3 className="">subtext something something</h3>
+        </div>
+
+        
       </div>
-      <div className="flex items-center justify-center mt-10">
-        <TaskCard taskName="Test" taskStatus="Completed" taskIcon="Cake"/>
+      <div className="flex flex-col mt-8 items-center justify-center text-center">
+        {mockTasks.map((task, index) => (
+          <TaskCard 
+            taskName={task.taskName}
+            taskDescription={task.taskDescription}
+            taskStatus={task.taskStatus}
+            taskIcon={task.taskIcon}
+            clickFunction={()=> toggleTaskDetails(`mock-${index}`)}
+          />
+        ))}
+
       </div >
       
-
+    
     </div>
   )
 }
--- task-board-app/src/components/TaskCard.tsx
+++ task-board-app/src/components/TaskCard.tsx
@@ -2,7 +2,6 @@ import {useState} from 'react'
 import { type TaskStatus } from '../types/taskType';
 import './TaskCard.css'
 import SetIcons from './SetIcons';
-import {Routes} from 'react-router'
 
 
 
@@ -11,39 +10,42 @@ interface TaskCardProps {
     taskDescription?: string | null; 
     taskIcon: string; 
     taskStatus: TaskStatus;
+    clickFunction: () => void; 
 }
 
-export function TaskCard({ taskName, taskDescription, taskIcon, taskStatus }: TaskCardProps) {
+export function TaskCard({ taskName, taskDescription, taskIcon, taskStatus, clickFunction }: TaskCardProps) {
+
     taskIcon = taskIcon || "Circle"; 
-    let bgcolor = taskStatus === "Completed" ? "bg-green-500" 
-                 : taskStatus === "In Progress" ? "bg-yellow-500"   
-                 : taskStatus === "To Do" ? "bg-gray-500" 
-                 : taskStatus === "Cancelled" ? "bg-red-500"
-                 : "bg-gray-200"; 
+    let bgcolor = taskStatus === "Completed" ? "bg-green-300" 
+                 : taskStatus === "In Progress" ? "bg-yellow-200"   
+                 : taskStatus === "To Do" ? "bg-gray-300" 
+                 : taskStatus === "Cancelled" ? "bg-red-200"
+                 : "bg-gray-400"; 
     let iconColor = taskStatus === "Completed" ? "green" 
-                 : taskStatus === "In Progress" ? "yellow"   
+                 : taskStatus === "In Progress" ? "orange"   
                  : taskStatus === "To Do" ? "black" 
                  : taskStatus === "Cancelled" ? "red"
                  : "gray"; 
 
     return (
-        <div className={`flex items-center gap-4 rounded-xl p-4 shadow-sm transition hover:shadow-gray-400 ${bgcolor}`} >
-            <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-gray-200">
+        <button className={`flex items-center gap-4 rounded-xl p-4 shadow-sm transition hover:shadow-gray-400 mb-3 hover:border-white ${bgcolor}`}
+            onClick={clickFunction}
+        >
+            <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-white">
                 <SetIcons iconName={taskIcon} color={iconColor}/>
             </div>
-            <div className="flex flex-col w-xs">
-                <div>
-                    <h3 className="text-sm font-semibold text-gray-900">{taskName}</h3>
+            <div className={`flex flex-row w-xs justify-between ${taskDescription ? "" : ""} h-full`}>
+                <div className="flex flex-col ml-3 text-left ">
+                    <h3 className="flex text-sm font-semibold text-gray-900">{taskName}</h3>
+                    {taskDescription && (
+                        <p className="flex mt-1 text-sm text-gray-600">{taskDescription}</p>
+                    )}
                 </div>
-                {taskDescription && (
-                    <div>
-
-                    </div>
-                )}
-                <div>
-
+                
+                <div className="flex w-10 h-10 shrink-0 items-center justify-center rounded-lg">
+                    <SetIcons iconName="Activity" color={iconColor}/>
                 </div>
             </div>
-        </div>
+        </button>
     )
 }
